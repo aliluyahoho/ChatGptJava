0e730f63770cb7fae781abdc717ff151d7f21cd7
alia <aliluyahoho@163.com>
2023-05-11 14:48:19 +0800

    add MP_verify

--- src/main/java/com/tencent/wxcloudrun/controller/IndexController.java
+++ src/main/java/com/tencent/wxcloudrun/controller/IndexController.java
@@ -2,6 +2,9 @@ package com.tencent.wxcloudrun.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.io.IOException;
 
 /**
  * index控制器
@@ -10,13 +13,20 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 public class IndexController {
 
-  /**
-   * 主页页面
-   * @return API response html
-   */
-  @GetMapping
-  public String index() {
-    return "index";
-  }
+    /**
+     * 主页页面
+     *
+     * @return API response html
+     */
+    @GetMapping
+    public String index() {
+        return "index";
+    }
+
+    @ResponseBody
+    @GetMapping("/MP_verify_P97WLFQjVODXzc3j.txt")
+    public String getData() throws IOException {
+        return "P97WLFQjVODXzc3j";
+    }
 
 }
